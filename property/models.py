5add408136535c6ae95d2162626045c4703af923
djeck1432 <djeck1432>
2020-01-24 20:21:05 +0100

    create models.like

--- property/models.py
+++ property/models.py
@@ -25,6 +25,7 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
 
     new_building = models.NullBooleanField("Новостройка", null=True)
+    like = models.ManyToManyField(User, verbose_name="Кто лайкнул:", null=True, blank=True)
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
