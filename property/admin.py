60b677ba8660cc27072c88155945656739c8c676
djeck1432 <djeck1432>
2020-01-24 20:17:44 +0100

    create Complain.models

--- property/admin.py
+++ property/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complain
+
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town','address','owner']
+    search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address','price','new_building','construction_year','town']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
-    list_filter = ['new_building','rooms_number','has_balcony','active']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony', 'active']
 
-admin.site.register(Flat,FlatAdmin)
+
+class ComplainAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user', 'flat']
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complain, ComplainAdmin)
