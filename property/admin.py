fde700a51c1e6ef3d66df4fe144efa96e0ffa4af
djeck1432 <djeck1432>
2020-01-29 20:10:05 +0100

    delete Flat.owner, Flat.owners_phonesmuber

--- property/admin.py
+++ property/admin.py
@@ -4,9 +4,9 @@ from .models import Flat, Complain, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'owner_phone_pure', 'new_building', 'construction_year', 'town']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony', 'active']
     raw_id_fields = ['like']
